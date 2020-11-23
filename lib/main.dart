07302a66d14dff03e1bea5158044be66c8926447
moricho <ikeda.morito@gmail.com>
2020-11-23 14:42:51 +0900

    build food items

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     height: MediaQuery.of(context).size.height - 300.0,
                     child: ListView(
                       children: [
-
+                        _buildFoodItem('assets/images/Spring.jpeg', 'Spring bowl', '\$24.00'),
+                        _buildFoodItem('assets/images/Green.jpeg', 'Green bowl', '\$24.00'),
+                        _buildFoodItem('assets/images/Cobb.jpeg', 'Cobb bowl', '\$24.00'),
+                        _buildFoodItem('assets/images/Salmon.jpeg', 'Salmon bowl', '\$24.00'),
                       ]
                     )
                   )
@@ -108,8 +111,60 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Widget _buildFoodItem(String imgPath, String name, String price) {
+Widget _buildFoodItem(String imgPath, String foodName, String price) {
   return Padding(
     padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+    child: InkWell(
+      onTap: () {
+
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            child: Row(
+              children: [
+                Hero(
+                  tag: imgPath,
+                  child: Image(
+                    image: AssetImage(imgPath),
+                    fit: BoxFit.cover,
+                    height: 75.0,
+                    width: 75.0,
+                  ),
+                ),
+                SizedBox(width: 10.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      foodName,
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      )
+                    ),
+                    Text(
+                      price,
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 15.0,
+                        color: Colors.grey,
+                      )
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.add),
+            color: Colors.black,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    ),
   );
 }
